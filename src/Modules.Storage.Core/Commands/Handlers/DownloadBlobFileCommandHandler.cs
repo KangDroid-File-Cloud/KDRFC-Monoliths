3dbab576ad37050be339c8c35acb1e159cc624e7
KangDroid <hyunwoo286@gmail.com>
2023-03-05 14:48:31 +0900

    [Feature] Storage: Change Download API To use Blob Access Token

--- src/Modules.Storage.Core/Commands/Handlers/DownloadBlobFileCommandHandler.cs
+++ src/Modules.Storage.Core/Commands/Handlers/DownloadBlobFileCommandHandler.cs
@@ -1,40 +1,38 @@
 using System.Net;
 using MediatR;
 using Modules.Storage.Core.Abstractions;
-using Modules.Storage.Core.Extensions;
 using Modules.Storage.Core.Models;
+using Modules.Storage.Core.Models.Data;
+using Modules.Storage.Core.Models.Responses;
 using MongoDB.Bson;
 using MongoDB.Driver.GridFS;
+using Shared.Core.Abstractions;
 using Shared.Core.Exceptions;
 
 namespace Modules.Storage.Core.Commands.Handlers;
 
 public class DownloadBlobFileCommandHandler : IRequestHandler<DownloadBlobFileCommand, GridFSDownloadStream<ObjectId>>
 {
+    private readonly ICacheService _cacheService;
     private readonly IGridFsRepository<BlobFile> _gridFsRepository;
 
-    public DownloadBlobFileCommandHandler(IGridFsRepository<BlobFile> gridFsRepository)
+    public DownloadBlobFileCommandHandler(IGridFsRepository<BlobFile> gridFsRepository, ICacheService cacheService)
     {
         _gridFsRepository = gridFsRepository;
+        _cacheService = cacheService;
     }
 
     public async Task<GridFSDownloadStream<ObjectId>> Handle(DownloadBlobFileCommand request,
                                                              CancellationToken cancellationToken)
     {
-        // Check whether blobFile exists.
-        var blobFile = await _gridFsRepository.GetFileById(request.BlobId) ??
-                       throw new ApiException(HttpStatusCode.NotFound, $"Cannot find blob: {request.BlobId}");
+        var targetKey =
+            await _cacheService.GetItemAsync<BlobEligibleResponse>(StorageCacheKeys.TempBlobDownloadKey(request.BlobId))
+            ?? throw new ApiException(HttpStatusCode.Unauthorized, $"Cannot access blob {request.BlobId}");
 
-        // Check whether blob is user's one.
-        if (blobFile.ToBlobFile().OwnerId != request.UserId)
+        // Check whether Blob Access Token matches
+        if (targetKey.Token != request.BlobAccessToken)
         {
-            throw new ApiException(HttpStatusCode.Forbidden, $"Blob {request.BlobId} is not user's one!");
-        }
-
-        // Check whether blob is not a file.
-        if (blobFile.ToBlobFile().BlobFileType == BlobFileType.Folder)
-        {
-            throw new ApiException(HttpStatusCode.BadRequest, $"Blob {request.BlobId} is not a file!");
+            throw new ApiException(HttpStatusCode.Unauthorized, $"Cannot access blob {request.BlobId}");
         }
 
         return await _gridFsRepository.OpenDownloadStreamAsync(request.BlobId, cancellationToken);
